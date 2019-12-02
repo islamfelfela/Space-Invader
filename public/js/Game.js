02f05bd135a93b19fe0365a94b37c47b3995ea2c
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2019-12-02 20:58:08 +0200

    adding score

--- public/js/Game.js
+++ public/js/Game.js
@@ -2,6 +2,7 @@ var mainContainer = document.getElementsByTagName('canvas')[0];
 var ctx = mainContainer.getContext('2d');
 mainContainer.height = window.innerHeight;
 mainContainer.width = window.innerWidth;
+ctx.font = '16px serif';
 
 var player = new Player(40,105,'assets/Missile_01.png',100,200);
 
@@ -16,6 +17,13 @@ document.addEventListener('click',function (event) {
 
 function distacne(x1,y1,x2,y2) {
     return Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
+}
+
+function displayScore(score) {
+    console.log(score)
+    ctx.fillStyle = "white";
+    ctx.clearRect(mainContainer.width-100,mainContainer.height-30,500,500)
+    ctx.fillText("score : "+score,mainContainer.width-100,mainContainer.height-20)
 }
 
 var enemies = []
@@ -33,11 +41,16 @@ setInterval(function () {
 setInterval(function () {
     for(var i = 0;i<enemies.length;++i)
     {
-        if(enemies[i].y>mainContainer.height+500)
+        if(enemies[i].y > mainContainer.height+500)
         {
             enemies[i].kill()
             enemies.splice(i,1);
             continue;
+        }
+
+        if(distacne(player.x,player.y,enemies[i].x,enemies[i].y)<40)
+        {
+
         }
 
         for(var j = 0;j<player.bullets.length;++j)
@@ -50,11 +63,13 @@ setInterval(function () {
             }
             if(distacne(enemies[i].x,enemies[i].y,player.bullets[j].x,player.bullets[j].y)<40)
             {
-            console.log(distacne(enemies[i].x,enemies[i].y,player.bullets[j].x,player.bullets[j].y))
+            //console.log(distacne(enemies[i].x,enemies[i].y,player.bullets[j].x,player.bullets[j].y))
                 enemies[i].kill()
                 player.bullets[j].kill()
                 enemies.splice(i,1);
                 player.bullets.splice(j,1);
+                player.score+=10;
+                displayScore(player.score);
             }
         }
     }
